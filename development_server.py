96bc1850ed867a6c209ecfd15b6d634b3400e721
Artyom <40771957+Artyom7638@users.noreply.github.com>
2020-04-25 21:27:40 +0300

    Update development_server.py

--- development_server.py
+++ development_server.py
@@ -1,7 +1,8 @@
 from src import app
 import os
-#int(p) = os.environ.get('PORT')
+
+
+p = int(os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
-    app.run()
-    #app.run(port=p)
+    app.run(port=p)
