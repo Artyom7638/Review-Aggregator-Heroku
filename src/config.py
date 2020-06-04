2b2b10bef8dcdd5710de2f06a567d0c3255b31d7
Artyom <40771957+Artyom7638@users.noreply.github.com>
2020-06-04 20:29:27 +0300

    test change

--- src/config.py
+++ src/config.py
@@ -25,3 +25,4 @@ class Config:
     MAIL_PASSWORD = 'password'
     EMAIL_ADDRESS = 'name@gmail.com'  # необходимо чтоб знать с какой почты отправлять
     EMAIL_CONFIRMATIONS_DISABLED = True
+    TEST = True
