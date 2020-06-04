34ca263fbfb55e54cd22b390822416d74349b078
Artyom <40771957+Artyom7638@users.noreply.github.com>
2020-06-04 19:14:09 +0300

    Update production_server.py

--- production_server.py
+++ production_server.py
@@ -7,8 +7,8 @@ from src import app
 
 port = int(os.environ.get('PORT', 8100))
 server = WSGIServer(('0.0.0.0', port), app)
-# adapter = BuiltinSSLAdapter(certificate="certificate.pem", private_key="certificate_private_key.pem")
-adapter = pyOpenSSLAdapter(certificate="certificate.pem", private_key="certificate_private_key.pem")
+adapter = BuiltinSSLAdapter(certificate="certificate.pem", private_key="certificate_private_key.pem")
+# adapter = pyOpenSSLAdapter(certificate="certificate.pem", private_key="certificate_private_key.pem")
 
 server.ssl_adapter = adapter
 
