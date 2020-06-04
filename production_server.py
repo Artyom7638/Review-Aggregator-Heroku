13b11732bd617b50156fa675f07a4e9fbead78a5
Artyom <40771957+Artyom7638@users.noreply.github.com>
2020-06-04 18:40:59 +0300

    Update production_server.py

--- production_server.py
+++ production_server.py
@@ -1,10 +1,12 @@
+import os
 from cheroot.wsgi import Server as WSGIServer
 from cheroot.ssl.builtin import BuiltinSSLAdapter
 from cheroot.ssl.pyopenssl import pyOpenSSLAdapter
 from src import app
 
 
-server = WSGIServer(('0.0.0.0', 8100), app)
+port = int(os.environ.get('PORT', 8100))
+server = WSGIServer(('0.0.0.0', port), app)
 # adapter = BuiltinSSLAdapter(certificate="certificate.pem", private_key="certificate_private_key.pem")
 adapter = pyOpenSSLAdapter(certificate="certificate.pem", private_key="certificate_private_key.pem")
 
